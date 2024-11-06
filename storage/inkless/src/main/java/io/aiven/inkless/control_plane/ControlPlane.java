8be9282be238b08d516b83b3b8d845b5aa88ee13
Ivan Yurchenko <ivan@ivanyu.me>
2024-11-06 07:50:22 +0200

    Add Inkless subproject (#13)
    The Checkstyle is now disabled for the subproject, but must be enabled once we figure out how to add amendments to the main Checkstyle config.

---
+++ storage/inkless/src/main/java/io/aiven/inkless/control_plane/ControlPlane.java
@@ -0,0 +1,5 @@
+// Copyright (c) 2024 Aiven, Helsinki, Finland. https://aiven.io/
+package io.aiven.inkless.control_plane;
+
+public class ControlPlane {
+}
---
+++ storage/inkless/src/test/java/io/aiven/inkless/control_plane/ControlPlaneTest.java
@@ -0,0 +1,11 @@
+// Copyright (c) 2024 Aiven, Helsinki, Finland. https://aiven.io/
+package io.aiven.inkless.control_plane;
+
+import org.junit.jupiter.api.Test;
+
+class ControlPlaneTest {
+    @Test
+    void itWorks() {
+        int a = 1;
+    }
+}
