43a39784c4541863f6b03139d543f3f5b916d632
Ivan Yurchenko <ivanyu@aiven.io>
2024-11-13 12:33:55 +0200

    Track requests in BatchBuffer (#30)
    This will allow matching commitment result to the original requests.

--- storage/inkless/src/main/java/io/aiven/inkless/produce/BatchBuffer.java
+++ storage/inkless/src/main/java/io/aiven/inkless/produce/BatchBuffer.java
@@ -15,9 +15,14 @@ import io.aiven.inkless.control_plane.CommitBatchRequest;
 class BatchBuffer {
     private final AtomicReference<List<BatchHolder>> batches = new AtomicReference<>(new ArrayList<>());
 
-    void addBatch(final TopicPartition topicPartition, final RecordBatch batch) {
+    private boolean closed = false;
+
+    void addBatch(final TopicPartition topicPartition, final RecordBatch batch, final int requestId) {
+        if (closed) {
+            throw new IllegalStateException("Already closed");
+        }
         batches.getAndUpdate(list -> {
-            list.add(new BatchHolder(topicPartition, batch));
+            list.add(new BatchHolder(topicPartition, batch, requestId));
             return list;
         });
     }
@@ -35,21 +40,32 @@ class BatchBuffer {
         final ByteBuffer byteBuffer = ByteBuffer.allocate(totalSize);
 
         final List<CommitBatchRequest> commitBatchRequests = new ArrayList<>();
+        final List<Integer> requestIds = new ArrayList<>();
         for (final BatchHolder batchHolder : currentBatchHolders) {
             final int offset = byteBuffer.position();
             commitBatchRequests.add(new CommitBatchRequest(
                 batchHolder.topicPartition(), offset, batchHolder.batch.sizeInBytes(), batchHolder.numberOfRecords()
             ));
+            requestIds.add(batchHolder.requestId);
             batchHolder.batch.writeTo(byteBuffer);
         }
-        return new CloseResult(commitBatchRequests, byteBuffer.array());
+
+        closed = true;
+        return new CloseResult(commitBatchRequests, requestIds, byteBuffer.array());
     }
 
+    /**
+     * The result of closing the buffer.
+     * @param commitBatchRequests commit batch requests matching in order the batches in {@code data}.
+     * @param requestIds produce request IDs matching in order the batches in {@code data}.
+     */
     record CloseResult(List<CommitBatchRequest> commitBatchRequests,
+                       List<Integer> requestIds,
                        byte[] data) {}
 
     record BatchHolder(TopicPartition topicPartition,
-                       RecordBatch batch) {
+                       RecordBatch batch,
+                       int requestId) {
         long numberOfRecords() {
             return batch.nextOffset() - batch.baseOffset();
         }
--- storage/inkless/src/test/java/io/aiven/inkless/produce/BatchBufferTest.java
+++ storage/inkless/src/test/java/io/aiven/inkless/produce/BatchBufferTest.java
@@ -3,13 +3,9 @@ package io.aiven.inkless.produce;
 
 import java.nio.ByteBuffer;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.compress.Compression;
-import org.apache.kafka.common.record.DefaultRecordBatch;
 import org.apache.kafka.common.record.MemoryRecords;
 import org.apache.kafka.common.record.RecordBatch;
 import org.apache.kafka.common.record.SimpleRecord;
@@ -19,6 +15,7 @@ import io.aiven.inkless.control_plane.CommitBatchRequest;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BatchBufferTest {
     private static final TopicPartition T0P0 = new TopicPartition("topic0", 0);
@@ -43,12 +40,13 @@ class BatchBufferTest {
     void singleBatch() {
         final RecordBatch batch = createBatch(T0P0 + "-0", T0P0 + "-1", T0P0 + "-2");
         final BatchBuffer buffer = new BatchBuffer();
-        buffer.addBatch(T0P0, batch);
+        buffer.addBatch(T0P0, batch, 0);
 
         final BatchBuffer.CloseResult result = buffer.close();
         assertThat(result.commitBatchRequests()).containsExactly(
             new CommitBatchRequest(T0P0, 0, batch.sizeInBytes(), 3)
         );
+        assertThat(result.requestIds()).containsExactly(0);
         assertThat(result.data()).containsExactly(batchToBytes(batch));
     }
 
@@ -69,15 +67,17 @@ class BatchBufferTest {
         final RecordBatch t1p0b2 = createBatch(T1P0 + "-2");
 
         final int batchSize = t0p0b0.sizeInBytes();  // expecting it to be same everywhere
-        buffer.addBatch(T0P0, t0p0b0);
-        buffer.addBatch(T1P0, t1p0b0);
-        buffer.addBatch(T0P0, t0p0b1);
-        buffer.addBatch(T1P0, t1p0b1);
-        buffer.addBatch(T0P1, t0p1b0);
-        buffer.addBatch(T0P0, t0p0b2);
-        buffer.addBatch(T0P1, t0p1b1);
-        buffer.addBatch(T0P1, t0p1b2);
-        buffer.addBatch(T1P0, t1p0b2);
+        buffer.addBatch(T0P0, t0p0b0, 0);
+        buffer.addBatch(T1P0, t1p0b0, 0);
+        buffer.addBatch(T0P0, t0p0b1, 0);
+
+        buffer.addBatch(T1P0, t1p0b1, 1);
+        buffer.addBatch(T0P1, t0p1b0, 1);
+        buffer.addBatch(T0P0, t0p0b2, 1);
+
+        buffer.addBatch(T0P1, t0p1b1, 2);
+        buffer.addBatch(T0P1, t0p1b2, 2);
+        buffer.addBatch(T1P0, t1p0b2, 2);
 
         // Here batches are sorted.
         final BatchBuffer.CloseResult result = buffer.close();
@@ -92,12 +92,17 @@ class BatchBufferTest {
             new CommitBatchRequest(T1P0, batchSize*7, batchSize, 1),
             new CommitBatchRequest(T1P0, batchSize*8, batchSize, 1)
         );
+        assertThat(result.requestIds()).containsExactly(
+            0, 0, 1,
+            1, 2, 2,
+            0, 1, 2
+        );
 
         // Here batch data are sorted too.
         final ByteBuffer expectedBytes = ByteBuffer.allocate(
-            t0p0b0.sizeInBytes() + t0p0b2.sizeInBytes() + t0p0b1.sizeInBytes()
-            + t0p1b0.sizeInBytes() + t0p1b2.sizeInBytes() + t0p1b1.sizeInBytes()
-            + t1p0b0.sizeInBytes() + t1p0b2.sizeInBytes() + t1p0b1.sizeInBytes()
+            t0p0b0.sizeInBytes() + t0p0b1.sizeInBytes() + t0p0b2.sizeInBytes()
+            + t0p1b0.sizeInBytes() + t0p1b1.sizeInBytes() + t0p1b2.sizeInBytes()
+            + t1p0b0.sizeInBytes() + t1p0b1.sizeInBytes() + t1p0b2.sizeInBytes()
         );
         t0p0b0.writeTo(expectedBytes);
         t0p0b1.writeTo(expectedBytes);
@@ -112,24 +117,22 @@ class BatchBufferTest {
     }
 
     @Test
-    void worksAfterClosing() {
+    void notWorksAfterClosing() {
         final BatchBuffer buffer = new BatchBuffer();
 
         final RecordBatch batch1 = createBatch(T0P0 + "-0");
-        buffer.addBatch(T0P0, batch1);
+        buffer.addBatch(T0P0, batch1, 0);
         final BatchBuffer.CloseResult result1 = buffer.close();
         assertThat(result1.commitBatchRequests()).containsExactly(
             new CommitBatchRequest(T0P0, 0, batch1.sizeInBytes(), 1)
         );
         assertThat(result1.data()).containsExactly(batchToBytes(batch1));
+        assertThat(result1.requestIds()).containsExactly(0);
 
         final RecordBatch batch2 = createBatch(T1P0 + "-0-longer");
-        buffer.addBatch(T1P0, batch2);
-        final BatchBuffer.CloseResult result2 = buffer.close();
-        assertThat(result2.commitBatchRequests()).containsExactly(
-            new CommitBatchRequest(T1P0, 0, batch2.sizeInBytes(), 1)
-        );
-        assertThat(result2.data()).containsExactly(batchToBytes(batch2));
+        assertThatThrownBy(() -> buffer.addBatch(T1P0, batch2, 1))
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessage("Already closed");
     }
 
     RecordBatch createBatch(String ...content) {
