de96154bbe91e85c1e1cdc2ab0e78f928e4c525f
Ivan Yurchenko <ivanyu@aiven.io>
2024-11-19 13:42:04 +0200

    Add Inkless config for produce path (#42)

--- storage/inkless/src/main/java/io/aiven/inkless/config/InklessConfig.java
+++ storage/inkless/src/main/java/io/aiven/inkless/config/InklessConfig.java
@@ -1,6 +1,7 @@
 // Copyright (c) 2024 Aiven, Helsinki, Finland. https://aiven.io/
 package io.aiven.inkless.config;
 
+import java.time.Duration;
 import java.util.Map;
 
 import org.apache.kafka.common.config.AbstractConfig;
@@ -12,12 +13,80 @@ import io.aiven.inkless.storage_backend.common.StorageBackend;
 public class InklessConfig extends AbstractConfig {
     private static final String PREFIX = "inkless.";
 
+    private static final String OBJECT_KEY_PREFIX_CONFIG = "object.key.prefix";
+    private static final String OBJECT_KEY_PREFIX_DOC = "The object storage key prefix.";
+
+    private static final String PRODUCE_PREFIX = "produce.";
+
+    private static final String PRODUCE_COMMIT_INTERVAL_MS_CONFIG = PRODUCE_PREFIX + "commit.interval.ms";
+    private static final String PRODUCE_COMMIT_INTERVAL_MS_DOC = "The interval with which produced data are committed.";
+    private static final int PRODUCE_COMMIT_INTERVAL_MS_DEFAULT = 250;
+
+    private static final String PRODUCE_BUFFER_MAX_BYTES_CONFIG = PRODUCE_PREFIX + "buffer.max.bytes";
+    private static final String PRODUCE_BUFFER_MAX_BYTES_DOC = "The max size of the buffer to accumulate produce requests. "
+        + "This is a best effort limit that cannot always be strictly enforced.";
+    private static final int PRODUCE_BUFFER_MAX_BYTES_DEFAULT = 8 * 1024 * 1024;  // 8 MiB
+
+    private static final String PRODUCE_MAX_UPLOAD_ATTEMPTS_CONFIG = PRODUCE_PREFIX + "max.upload.attempts";
+    private static final String PRODUCE_MAX_UPLOAD_ATTEMPTS_DOC = "The max number of attempts to upload a file to the object storage.";
+    private static final int PRODUCE_MAX_UPLOAD_ATTEMPTS_DEFAULT = 3;
+
+    private static final String PRODUCE_UPLOAD_BACKOFF_MS_CONFIG = PRODUCE_PREFIX + "upload.backoff.ms";
+    private static final String PRODUCE_UPLOAD_BACKOFF_MS_DOC = "The number of millisecond to back off for before the next upload attempt.";
+    private static final int PRODUCE_UPLOAD_BACKOFF_MS_DEFAULT = 10;
+
     private static final String STORAGE_PREFIX = "storage.";
+
     private static final String STORAGE_BACKEND_CLASS_CONFIG = STORAGE_PREFIX + "backend.class";
     private static final String STORAGE_BACKEND_CLASS_DOC = "The storage backend implementation class";
 
     public static ConfigDef configDef() {
         final ConfigDef configDef = new ConfigDef();
+
+        configDef.define(
+            OBJECT_KEY_PREFIX_CONFIG,
+            ConfigDef.Type.STRING,
+            "",
+            new ConfigDef.NonNullValidator(),
+            ConfigDef.Importance.MEDIUM,
+            OBJECT_KEY_PREFIX_DOC
+        );
+
+        configDef.define(
+            PRODUCE_COMMIT_INTERVAL_MS_CONFIG,
+            ConfigDef.Type.INT,
+            PRODUCE_COMMIT_INTERVAL_MS_DEFAULT,
+            ConfigDef.Range.atLeast(1),
+            ConfigDef.Importance.HIGH,
+            PRODUCE_COMMIT_INTERVAL_MS_DOC
+        );
+
+        configDef.define(
+            PRODUCE_BUFFER_MAX_BYTES_CONFIG,
+            ConfigDef.Type.INT,
+            PRODUCE_BUFFER_MAX_BYTES_DEFAULT,
+            ConfigDef.Range.atLeast(1),
+            ConfigDef.Importance.HIGH,
+            PRODUCE_BUFFER_MAX_BYTES_DOC
+        );
+
+        configDef.define(
+            PRODUCE_MAX_UPLOAD_ATTEMPTS_CONFIG,
+            ConfigDef.Type.INT,
+            PRODUCE_MAX_UPLOAD_ATTEMPTS_DEFAULT,
+            ConfigDef.Range.atLeast(1),
+            ConfigDef.Importance.MEDIUM,
+            PRODUCE_MAX_UPLOAD_ATTEMPTS_DOC
+        );
+
+        configDef.define(
+            PRODUCE_UPLOAD_BACKOFF_MS_CONFIG,
+            ConfigDef.Type.INT,
+            PRODUCE_UPLOAD_BACKOFF_MS_DEFAULT,
+            ConfigDef.Range.atLeast(0),
+            ConfigDef.Importance.MEDIUM,
+            PRODUCE_UPLOAD_BACKOFF_MS_DOC
+        );
 
         configDef.define(
             STORAGE_BACKEND_CLASS_CONFIG,
@@ -37,7 +106,10 @@ public class InklessConfig extends AbstractConfig {
     // Visible for testing
     InklessConfig(final Map<String, ?> props) {
         super(configDef(), props);
-        System.out.println(props);
+    }
+
+    public String objectKeyPrefix() {
+        return getString(OBJECT_KEY_PREFIX_CONFIG);
     }
 
     public StorageBackend storage() {
@@ -45,5 +117,20 @@ public class InklessConfig extends AbstractConfig {
         final StorageBackend storage = Utils.newInstance(storageClass, StorageBackend.class);
         storage.configure(this.originalsWithPrefix(STORAGE_PREFIX));
         return storage;
+    }
+
+    public Duration commitInterval() {
+        return Duration.ofMillis(getInt(PRODUCE_COMMIT_INTERVAL_MS_CONFIG));
+    }
+
+    public int produceBufferMaxBytes() {
+        return getInt(PRODUCE_BUFFER_MAX_BYTES_CONFIG);
+    }
+
+    public int produceMaxUploadAttempts() {
+        return getInt(PRODUCE_MAX_UPLOAD_ATTEMPTS_CONFIG);
+    }
+    public Duration produceUploadBackoff() {
+        return Duration.ofMillis(getInt(PRODUCE_UPLOAD_BACKOFF_MS_CONFIG));
     }
 }
--- storage/inkless/src/test/java/io/aiven/inkless/config/InklessConfigTest.java
+++ storage/inkless/src/test/java/io/aiven/inkless/config/InklessConfigTest.java
@@ -1,13 +1,127 @@
 // Copyright (c) 2024 Aiven, Helsinki, Finland. https://aiven.io/
 package io.aiven.inkless.config;
 
+import java.time.Duration;
+import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigException;
 
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InklessConfigTest {
+    @Test
+    void publicConstructor() {
+        final InklessConfig config = new InklessConfig(new AbstractConfig(new ConfigDef(), Map.of(
+            "inkless.object.key.prefix", "prefix/",
+            "inkless.produce.commit.interval.ms", "100",
+            "inkless.produce.buffer.max.bytes", "1024",
+            "inkless.produce.max.upload.attempts", "5",
+            "inkless.produce.upload.backoff.ms", "30",
+            "inkless.storage.backend.class", ConfigTestStorageBackend.class.getCanonicalName()
+        )));
+        assertThat(config.objectKeyPrefix()).isEqualTo("prefix/");
+        assertThat(config.commitInterval()).isEqualTo(Duration.ofMillis(100));
+        assertThat(config.produceBufferMaxBytes()).isEqualTo(1024);
+        assertThat(config.produceMaxUploadAttempts()).isEqualTo(5);
+        assertThat(config.produceUploadBackoff()).isEqualTo(Duration.ofMillis(30));
+        assertThat(config.storage()).isInstanceOf(ConfigTestStorageBackend.class);
+    }
+
+    @Test
+    void minimalConfig() {
+        final var config = new InklessConfig(
+            Map.of(
+                "storage.backend.class", ConfigTestStorageBackend.class.getCanonicalName()
+            )
+        );
+        assertThat(config.objectKeyPrefix()).isEqualTo("");
+        assertThat(config.commitInterval()).isEqualTo(Duration.ofMillis(250));
+        assertThat(config.produceBufferMaxBytes()).isEqualTo(8 * 1024 * 1024);
+        assertThat(config.produceMaxUploadAttempts()).isEqualTo(3);
+        assertThat(config.produceUploadBackoff()).isEqualTo(Duration.ofMillis(10));
+        assertThat(config.storage()).isInstanceOf(ConfigTestStorageBackend.class);
+    }
+
+    @Test
+    void fullConfig() {
+        final var config = new InklessConfig(
+            Map.of(
+                "object.key.prefix", "prefix/",
+                "produce.commit.interval.ms", "100",
+                "produce.buffer.max.bytes", "1024",
+                "produce.max.upload.attempts", "5",
+                "produce.upload.backoff.ms", "30",
+                "storage.backend.class", ConfigTestStorageBackend.class.getCanonicalName()
+            )
+        );
+        assertThat(config.objectKeyPrefix()).isEqualTo("prefix/");
+        assertThat(config.commitInterval()).isEqualTo(Duration.ofMillis(100));
+        assertThat(config.produceBufferMaxBytes()).isEqualTo(1024);
+        assertThat(config.produceMaxUploadAttempts()).isEqualTo(5);
+        assertThat(config.produceUploadBackoff()).isEqualTo(Duration.ofMillis(30));
+        assertThat(config.storage()).isInstanceOf(ConfigTestStorageBackend.class);
+    }
+
+    @Test
+    void objectKeyPrefixNull() {
+        final Map<String, String> config = new HashMap<>();
+        config.put("storage.backend.class", ConfigTestStorageBackend.class.getCanonicalName());
+        config.put("object.key.prefix", null);
+        assertThatThrownBy(() -> new InklessConfig(config))
+            .isInstanceOf(ConfigException.class)
+            .hasMessage("Invalid value null for configuration object.key.prefix: entry must be non null");
+    }
+
+    @Test
+    void produceCmmitIntervalZero() {
+        final Map<String, String> config = Map.of(
+            "storage.backend.class", ConfigTestStorageBackend.class.getCanonicalName(),
+            "produce.commit.interval.ms", "0"
+        );
+        assertThatThrownBy(() -> new InklessConfig(config))
+            .isInstanceOf(ConfigException.class)
+            .hasMessage("Invalid value 0 for configuration produce.commit.interval.ms: Value must be at least 1");
+    }
+
+    @Test
+    void produceBufferMaxBytesZero() {
+        final Map<String, String> config = Map.of(
+            "storage.backend.class", ConfigTestStorageBackend.class.getCanonicalName(),
+            "produce.buffer.max.bytes", "0"
+        );
+        assertThatThrownBy(() -> new InklessConfig(config))
+            .isInstanceOf(ConfigException.class)
+            .hasMessage("Invalid value 0 for configuration produce.buffer.max.bytes: Value must be at least 1");
+    }
+
+    @Test
+    void produceMaxUploadAttemptsZero() {
+        final Map<String, String> config = Map.of(
+            "storage.backend.class", ConfigTestStorageBackend.class.getCanonicalName(),
+            "produce.max.upload.attempts", "0"
+        );
+        assertThatThrownBy(() -> new InklessConfig(config))
+            .isInstanceOf(ConfigException.class)
+            .hasMessage("Invalid value 0 for configuration produce.max.upload.attempts: Value must be at least 1");
+    }
+
+    @Test
+    void produceMaxUploadBackoffMsNegative() {
+        final Map<String, String> config = Map.of(
+            "storage.backend.class", ConfigTestStorageBackend.class.getCanonicalName(),
+            "produce.upload.backoff.ms", "-1"
+        );
+        assertThatThrownBy(() -> new InklessConfig(config))
+            .isInstanceOf(ConfigException.class)
+            .hasMessage("Invalid value -1 for configuration produce.upload.backoff.ms: Value must be at least 0");
+    }
+
     @Test
     void objectStorageConfiguration() {
         final String backendClass = ConfigTestStorageBackend.class.getCanonicalName();
