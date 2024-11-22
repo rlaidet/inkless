a4fa866a4f8481378ff16bbddd1568182e6e69d0
Ivan Yurchenko <ivanyu@aiven.io>
2024-11-22 17:32:27 +0200

    Add in-memory storage implementation (#58)

---
+++ storage/inkless/src/main/java/io/aiven/inkless/storage_backend/in_memory/InMemoryStorage.java
@@ -0,0 +1,71 @@
+// Copyright (c) 2024 Aiven, Helsinki, Finland. https://aiven.io/
+package io.aiven.inkless.storage_backend.in_memory;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import io.aiven.inkless.common.ByteRange;
+import io.aiven.inkless.common.ObjectKey;
+import io.aiven.inkless.storage_backend.common.InvalidRangeException;
+import io.aiven.inkless.storage_backend.common.KeyNotFoundException;
+import io.aiven.inkless.storage_backend.common.StorageBackend;
+import io.aiven.inkless.storage_backend.common.StorageBackendException;
+
+import org.apache.commons.io.input.BoundedInputStream;
+
+/**
+ * The in-memory implementation of {@link StorageBackend}.
+ *
+ * <p>Useful for testing.
+ */
+public class InMemoryStorage implements StorageBackend {
+    private final ConcurrentHashMap<ObjectKey, byte[]> storage = new ConcurrentHashMap<>();
+
+    @Override
+    public void configure(final Map<String, ?> configs) {
+        // do nothing
+    }
+
+    @Override
+    public void upload(final ObjectKey key, final byte[] data) throws StorageBackendException {
+        Objects.requireNonNull(key, "key cannot be null");
+        Objects.requireNonNull(data, "data cannot be null");
+        storage.put(key, data);
+    }
+
+    @Override
+    public InputStream fetch(final ObjectKey key, final ByteRange range) throws StorageBackendException {
+        Objects.requireNonNull(key, "key cannot be null");
+        Objects.requireNonNull(range, "range cannot be null");
+
+        final byte[] data = storage.get(key);
+        if (data == null) {
+            throw new KeyNotFoundException(this, key);
+        }
+
+        if (range.offset() >= data.length) {
+            throw new InvalidRangeException("Range start offset " + range.offset()
+                + " is outside of data size " + data.length);
+        }
+
+        final ByteArrayInputStream inner = new ByteArrayInputStream(data);
+        inner.skip(range.offset());
+        return new BoundedInputStream(inner, range.size());
+    }
+
+    @Override
+    public void delete(final ObjectKey key) throws StorageBackendException {
+        Objects.requireNonNull(key, "key cannot be null");
+        storage.remove(key);
+    }
+
+    @Override
+    public void delete(final Set<ObjectKey> keys) throws StorageBackendException {
+        Objects.requireNonNull(keys, "keys cannot be null");
+        keys.forEach(storage::remove);
+    }
+}
---
+++ storage/inkless/src/test/java/io/aiven/inkless/storage_backend/in_memory/InMemoryStorageTest.java
@@ -0,0 +1,126 @@
+// Copyright (c) 2024 Aiven, Helsinki, Finland. https://aiven.io/
+package io.aiven.inkless.storage_backend.in_memory;
+
+import java.io.InputStream;
+import java.util.Set;
+
+import io.aiven.inkless.common.ByteRange;
+import io.aiven.inkless.common.ObjectKey;
+import io.aiven.inkless.common.PlainObjectKey;
+import io.aiven.inkless.storage_backend.common.InvalidRangeException;
+import io.aiven.inkless.storage_backend.common.KeyNotFoundException;
+import io.aiven.inkless.storage_backend.common.StorageBackendException;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InMemoryStorageTest {
+    static final PlainObjectKey OBJECT_KEY = new PlainObjectKey("a", "b");
+
+    @Test
+    void uploadNulls() {
+        final InMemoryStorage storage = new InMemoryStorage();
+        assertThatThrownBy(() -> storage.upload(null, new byte[0]))
+            .isInstanceOf(NullPointerException.class)
+            .hasMessage("key cannot be null");
+        assertThatThrownBy(() -> storage.upload(OBJECT_KEY, null))
+            .isInstanceOf(NullPointerException.class)
+            .hasMessage("data cannot be null");
+    }
+
+    @Test
+    void fetchNulls() {
+        final InMemoryStorage storage = new InMemoryStorage();
+        assertThatThrownBy(() -> storage.fetch(null, new ByteRange(0, 10)))
+            .isInstanceOf(NullPointerException.class)
+            .hasMessage("key cannot be null");
+        assertThatThrownBy(() -> storage.fetch(OBJECT_KEY, null))
+            .isInstanceOf(NullPointerException.class)
+            .hasMessage("range cannot be null");
+    }
+
+    @Test
+    void deleteNulls() {
+        final InMemoryStorage storage = new InMemoryStorage();
+        assertThatThrownBy(() -> storage.delete((ObjectKey) null))
+            .isInstanceOf(NullPointerException.class)
+            .hasMessage("key cannot be null");
+        assertThatThrownBy(() -> storage.delete((Set<ObjectKey>) null))
+            .isInstanceOf(NullPointerException.class)
+            .hasMessage("keys cannot be null");
+    }
+
+    @Test
+    void fetchNonExistent() {
+        final InMemoryStorage storage = new InMemoryStorage();
+        assertThatThrownBy(() -> storage.fetch(OBJECT_KEY, ByteRange.maxRange()))
+            .isInstanceOf(KeyNotFoundException.class);
+    }
+
+    @Test
+    void uploadAndFetch() throws StorageBackendException {
+        final InMemoryStorage storage = new InMemoryStorage();
+        final byte[] data = new byte[10];
+        storage.upload(OBJECT_KEY, data);
+
+        final InputStream fetch = storage.fetch(OBJECT_KEY, ByteRange.maxRange());
+
+        assertThat(fetch).hasBinaryContent(data);
+    }
+
+    @Test
+    void fetchRanged() throws StorageBackendException {
+        final InMemoryStorage storage = new InMemoryStorage();
+        final byte[] data = new byte[]{0, 1, 2, 3, 4, 5, 6, 7};
+        storage.upload(OBJECT_KEY, data);
+
+        final InputStream fetch1 = storage.fetch(OBJECT_KEY, new ByteRange(1, 2));
+        assertThat(fetch1).hasBinaryContent(new byte[]{1, 2});
+
+        final InputStream fetch2 = storage.fetch(OBJECT_KEY, new ByteRange(1, 100));
+        assertThat(fetch2).hasBinaryContent(new byte[]{1, 2, 3, 4, 5, 6, 7});
+    }
+
+    @Test
+    void fetchOutsideOfSize() throws StorageBackendException {
+        final InMemoryStorage storage = new InMemoryStorage();
+        final byte[] data = new byte[]{0, 1, 2, 3, 4, 5, 6, 7};
+        storage.upload(OBJECT_KEY, data);
+
+        assertThatThrownBy(() -> storage.fetch(OBJECT_KEY, new ByteRange(8, 1)))
+            .isInstanceOf(InvalidRangeException.class)
+            .hasMessage("Range start offset 8 is outside of data size 8");
+    }
+
+    @Test
+    void delete() throws StorageBackendException {
+        final InMemoryStorage storage = new InMemoryStorage();
+        final byte[] data = new byte[]{0, 1, 2, 3, 4, 5, 6, 7};
+        storage.upload(OBJECT_KEY, data);
+
+        final InputStream fetch = storage.fetch(OBJECT_KEY, new ByteRange(1, 2));
+        assertThat(fetch).hasBinaryContent(new byte[]{1, 2});
+
+        storage.delete(OBJECT_KEY);
+
+        assertThatThrownBy(() -> storage.fetch(OBJECT_KEY, ByteRange.maxRange()))
+            .isInstanceOf(KeyNotFoundException.class);
+    }
+
+    @Test
+    void deleteMany() throws StorageBackendException {
+        final InMemoryStorage storage = new InMemoryStorage();
+        final byte[] data = new byte[]{0, 1, 2, 3, 4, 5, 6, 7};
+        storage.upload(OBJECT_KEY, data);
+
+        final InputStream fetch = storage.fetch(OBJECT_KEY, new ByteRange(1, 2));
+        assertThat(fetch).hasBinaryContent(new byte[]{1, 2});
+
+        storage.delete(Set.of(OBJECT_KEY, new PlainObjectKey("un", "related")));
+
+        assertThatThrownBy(() -> storage.fetch(OBJECT_KEY, ByteRange.maxRange()))
+            .isInstanceOf(KeyNotFoundException.class);
+    }
+}
